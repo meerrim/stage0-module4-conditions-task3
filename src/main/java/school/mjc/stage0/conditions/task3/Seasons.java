65d7d9e2684325bc793e24413b41f0b30b4fbe26
Meerim Sadykbekova <sadykbekova.m@gmail.com>
2023-04-01 22:55:11 +0600

    Completed all tasks.

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -3,5 +3,18 @@ package school.mjc.stage0.conditions.task3;
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
 
+        if (month >= 3 && month <= 5) {
+            System.out.println("Spring");
+        } else if (month >= 6 && month <= 8) {
+            System.out.println("Summer");
+        }
+         else if (month >= 9 && month <= 11) {
+            System.out.println("Fall");
+        } else if (month == 1 || month == 2 || month == 12) {
+            System.out.println("Winter");
+        } else if (month >= 13 || month <= 0) {
+            System.out.println("wrong number!");
+        }
+
     }
 }
